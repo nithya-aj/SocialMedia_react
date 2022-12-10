430da66e7cba8eb689a190a9abc4dccbde1581b0
Nithya AJ <aj7nithya@gmail.com>
2022-12-11 03:28:47 +0530

    second commit- just started

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
 const app = express()
-app.use(bodyParser.json({ limit:'30mb', extended:true}))
-app.use(bodyParser.urlencoded({ limit:'30mb', extended:true}))
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() =>
+        app.listen(process.env.PORT, () =>
+            console.log(`listening at ${process.env.PORT}`)))
+    .catch((err) => console.log(err))
